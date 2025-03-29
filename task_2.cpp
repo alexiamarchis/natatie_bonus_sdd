9904495bb513e92c9730c1f010c5da71278aecbf
ioana735 <nitan.ioana@gmail.com>
2025-03-29 21:33:52 +0200

    m

--- task_2.cpp
+++ task_2.cpp
@@ -12,7 +12,7 @@ double task_2(int N, int M, const OrderedSet<Duck>&ducks, const OrderedSet<Lane>
     double mx=0;
      for(int i=0; i<M; i++)
        {
-        Duck d=ducks.getElem(i);
+        Duck d=ducks.getElem(i);  
         Lane l=lanes.getElem(i);
         double time=(2.0* l.getLength()/d.getSpeed());
         if(time>mx)
